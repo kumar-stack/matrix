13250216a415eee0b4678355387a5cfccb7d7428
kumar-stack <gk690661@gmail.com>
2022-07-12 19:45:59 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h2').style.display = 'block';
+    document.querySelector('h2').style.cursor = 'pointer';
+    document.querySelector('h2').style.backgroundColor = 'lightgreen';
+    document.querySelector('h2').addEventListener('click',function(){
+        document.querySelector('h2').style.color = 'green';
+    })
+})
+document.addEventListener('keydown',function(kp){
+    console.log(kp.key)
+})
+*/
+/*
+document.addEventListener('keydown',function(){
+    console.log('key is pressed')
+  })
+  */
+
+  
+  
